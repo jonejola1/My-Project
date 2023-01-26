80045343458493ab2741a3a3db539736401432db
janji <62443314+jonejola1@users.noreply.github.com>
2023-01-26 16:32:30 +0100

    changed: better text input

--- JS/loginView.js
+++ JS/loginView.js
@@ -6,7 +6,7 @@ function loginView() {
             login
         </h1>
         <input type="text" oninput="model.inputs.login.username = this.value" placeholder="username">
-        <input type="text" oninput="model.inputs.login.password = this.value" placeholder="password"> 
+        <input type="password" oninput="model.inputs.login.password = this.value" placeholder="password"> 
         <br>
         <button onclick="checkLogin(model.inputs.login.username, model.inputs.login.password)">Login</button>
         <button onclick="changePage('register')">Register</button>
--- JS/registerView.js
+++ JS/registerView.js
@@ -6,10 +6,10 @@ function registerView() {
                 Register
             </h1>
             <input type="text" oninput="model.inputs.registerUser.username = this.value" placeholder="Username">
-            <input type="text" oninput="model.inputs.registerUser.email = this.value" placeholder="E-mail"> 
+            <input type="email" oninput="model.inputs.registerUser.email = this.value" placeholder="E-mail"> 
             <br>
-            <input type="text" oninput=" model.inputs.registerUser.password = this.value " placeholder="Password">
-            <input type="text" oninput=" model.inputs.registerUser.confirm_password = this.value" placeholder="Confirm password"> 
+            <input type="password" oninput=" model.inputs.registerUser.password = this.value " placeholder="Password">
+            <input type="password" oninput=" model.inputs.registerUser.confirm_password = this.value" placeholder="Confirm password"> 
             <br>
             <button onclick="registerUser()">Register user</button>
             <button onclick="changePage('login')">Back to login</button>
